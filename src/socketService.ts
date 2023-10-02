c56d142f8ab0e57d477ef19461b58fc31fe5d840
rom guyer <romguyer@roms-MacBook-Pro.local>
2023-10-02 13:09:10 +0300

    added creds

--- src/socketService.ts
+++ src/socketService.ts
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client"
 
 const socket = io("https://onlinecoding-backend.onrender.com", {
-  transports: ['websocket']
+  transports: ['websocket'],
+  withCredentials: true
 });
 
 export default socket;
