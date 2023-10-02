2365627cc6b374d7a94436eea35d2d17df574635
rom guyer <romguyer@roms-MacBook-Pro.local>
2023-10-02 18:03:42 +0300

    disable auto connection

--- src/socketService.ts
+++ src/socketService.ts
@@ -2,7 +2,8 @@ import { io } from "socket.io-client"
 
 const socket = io("https://onlinecoding-backend.onrender.com", {
   transports: ['websocket'],
-  withCredentials: true
+  withCredentials: true,
+  autoConnect: false
 });
 
 export default socket;
